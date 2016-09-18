a26a5ddab15a278c98c9c534e9d67a99055bfc4a
Nianpeng Li <linianpeng@gmail.com>
2016-09-18 17:12:28 -0400

    bill date picker

--- app/assets/javascripts/controllers/bills/new.js
+++ app/assets/javascripts/controllers/bills/new.js
@@ -3,8 +3,22 @@ angular
   .controller('NewBillModalCtrl', ['$uibModalInstance', 'cases',
     function ($uibModalInstance, cases) {
       var $ctrl = this;
-      $ctrl.bill = { date: new Date() };
-      $ctrl.cases = cases;
+
+      $ctrl.today = function() {
+        $ctrl.bill.date = new Date();
+      };
+
+      $ctrl.openCalendar = function() {
+        $ctrl.calendar.opened = true;
+      };
+
+      $ctrl.dateOptions = {
+        maxDate: new Date(2032, 1, 1),
+        minDate: new Date(2012, 1, 1),
+        showWeeks: false,
+      };
+
+      $ctrl.dateFormat = 'yyyy-MM-dd';
 
       $ctrl.ok = function() {
         $uibModalInstance.close($ctrl.bill);
@@ -13,5 +27,14 @@ angular
       $ctrl.cancel = function() {
         $uibModalInstance.dismiss('cancel');
       };
+
+      $ctrl.init = function() {
+        $ctrl.bill = {};
+        $ctrl.cases = cases;
+        $ctrl.calendar = { opened: false };
+        $ctrl.today();
+      };
+
+      $ctrl.init();
     }
   ]);
